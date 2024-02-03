33a0b54b38fe691f7351a8f9217c66e849e8c627
Priyanka Devi <priyanka.devi@ui.ac.id>
2024-02-03 08:15:29 +0700

    TC1:Success login with valid username and password test case

---
+++ cypress/e2e/login feature/successlogin.cy.js
@@ -0,0 +1,16 @@
+describe('E2E Login to dashboard', () => {
+
+    it('input valid username dan password then Login', () => {
+
+      cy.visit('https://magento.softwaretestingboard.com/')
+      cy.contains("Sign In").click()
+      cy.url().should('include', '/customer/account/login') 
+      // <input name="login[username]" value="" autocomplete="off" id="email" type="email" class="input-text" title="Email" data-validate="{required:true, 'validate-email':true}" aria-required="true">
+      cy.get('#email').type("email4testing.ata@gmail.com")
+      // <input name="login[password]" type="password" autocomplete="off" class="input-text" id="pass" title="Password" data-validate="{required:true}" aria-required="true">
+      cy.get('#pass').type("email4testingQA")
+      cy.contains("Sign In").click()
+      cy.url().should('include', '/customer/account/') 
+    })
+
+  })
