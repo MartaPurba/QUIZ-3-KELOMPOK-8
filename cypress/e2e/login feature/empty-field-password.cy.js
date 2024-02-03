b39fa3049edad837834b6682e477f9bbc6745e6b
Priyanka Devi <priyanka.devi@ui.ac.id>
2024-02-03 16:42:30 +0700

    TC6: Submit empty field for password

---
+++ cypress/e2e/login feature/empty-field-password.cy.js
@@ -0,0 +1,17 @@
+describe('E2E Login to dashboard', () => {
+
+    it('Valid email field and empty password field then Login', () => {
+      cy.visit('https://magento.softwaretestingboard.com/')
+      cy.then(Cypress.session.clearCurrentSessionData)
+      cy.contains("Sign In").click()
+      cy.url().should('include', '/customer/account/login') 
+      cy.get('#email')
+      .should('have.attr', 'aria-required', 'true');
+      cy.get('#pass')
+      .should('have.attr', 'aria-required', 'true');
+      cy.get('#email').type("email4testing.ata@gmail.com")
+      cy.get('#send2').click()
+      cy.get('#pass-error').should('be.visible');
+    })
+
+  })
