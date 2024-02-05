0cc17fcae287237b28aadf62aaf597e31c7ed94e
Marta Purba <marta.purba06@gmail.com>
2024-02-05 11:11:00 +0700

    Create an Account with empty every field

--- cypress/e2e/Create-an-Account/empty-everyfield.cy.js
+++ cypress/e2e/Create-an-Account/empty-everyfield.cy.js
@@ -1,29 +1,30 @@
 describe('Create an account', () => {
   
-    it('passes', () => {
-      cy.visit('https://magento.softwaretestingboard.com/')
-      cy.contains("Create an Account").click()
-      cy.get('#firstname')
-      .should('have.attr', 'aria-required', 'true');
-      cy.get('[name="lastname"]')
-      .should('have.attr', 'aria-required', 'true');
-      cy.get('[name="email"]')
-      .should('have.attr', 'aria-required', 'true');
-      cy.get('#password')
-      .should('have.attr', 'aria-required', 'true');
-      cy.get('[name="password_confirmation"]')
-      .should('have.attr', 'aria-required', 'true');
-      cy.get('.action.submit.primary').click()
+  it('Creat an Account with empty every field', () => {
+    cy.visit('https://magento.softwaretestingboard.com/')
+    cy.contains("Create an Account").click()
+    cy.get('#firstname')
+    .should('have.attr', 'aria-required', 'true');
+    cy.get('[name="lastname"]')
+    .should('have.attr', 'aria-required', 'true');
+    cy.get('[name="email"]')
+    .should('have.attr', 'aria-required', 'true');
+    cy.get('#password')
+    .should('have.attr', 'aria-required', 'true');
+    cy.get('[name="password_confirmation"]')
+    .should('have.attr', 'aria-required', 'true');
+    cy.get('.action.submit.primary').click()
 
-      cy.get('#firstname-error').should('be.visible')
-      .should('have.text', 'This is a required field.');
-      cy.get('#lastname-error').should('be.visible')
-      .should('have.text', 'This is a required field.');
-      cy.get('#email_address-error').should('be.visible')
-      .should('have.text', 'This is a required field.');
-      cy.get('#password-error').should('be.visible')
-      .should('have.text', 'This is a required field.');
-      cy.get('#password-confirmation-error').should('be.visible')
-      .should('have.text', 'This is a required field.');
-    })
+    cy.get('#firstname-error').should('be.visible')
+    .should('have.text', 'This is a required field.');
+    cy.get('#lastname-error').should('be.visible')
+    .should('have.text', 'This is a required field.');
+    cy.get('#email_address-error').should('be.visible')
+    .should('have.text', 'This is a required field.');
+    cy.get('#password-error').should('be.visible')
+    .should('have.text', 'This is a required field.');
+    cy.get('#password-confirmation-error').should('be.visible')
+    .should('have.text', 'This is a required field.');
   })
+})
+
